bbb272497b2d9dc41c57e69e457579d318a6a125
John Garner <jdextergarneriv@gmail.com>
2020-11-12 14:48:58 -0500

    require api-routes in serverjs

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+require("./app/routing/api-routes.js")(app);
 require("./app/routing/html-routes.js")(app);
 
 app.listen(PORT, function () {
