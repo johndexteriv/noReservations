65e185a813154cceb5683d60818551ee30b83724
John Garner <jdextergarneriv@gmail.com>
2020-11-12 14:12:38 -0500

    created html file structure

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 8080;
 
 app.get("/", function (req, res) {
 	res.send("Hello World");
 });
 
-app.listen(3000);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(function (req, res) {
+	res.setHeader("Content-Type", "text/plain");
+	res.write("you posted:\n");
+	res.end(JSON.stringify(req.body, null, 2));
+});
+
+app.listen(PORT, function () {
+	console.log("APP LISTENING ON PORT: " + PORT);
+});
