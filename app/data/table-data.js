38a4a97204044ef3bfaf6dfe0005edba09427384
John Garner <jdextergarneriv@gmail.com>
2020-11-12 14:21:44 -0500

    created table array and waitinglist array

---
+++ app/data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+	{
+		customerName: "John",
+		customerEmail: "john@example.com",
+		customerId: "john7223",
+		phoneNuber: "000-000-0000",
+	},
+];
+
+module.exports = tableArray;
