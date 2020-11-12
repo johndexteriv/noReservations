8ee2de3989b257a0190a8c0ae4c1148b76510668
John Garner <jdextergarneriv@gmail.com>
2020-11-12 17:09:23 -0500

    created ajax get calls for tabledata and waitlistdata

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -8,4 +8,13 @@ module.exports = function (app) {
 	app.get("/api/waitlist", function (req, res) {
 		res.json(waitingListData);
 	});
+	app.post("/api/tables", function (req, res) {
+		if (tableData.length < 5) {
+			tableData.push(req.body);
+			res.json(true);
+		} else {
+			waitingListData.push(req.body);
+			res.json(false);
+		}
+	});
 };
