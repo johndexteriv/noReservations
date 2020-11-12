9669b07cb7a3ec831b990af91a05ebec91f53cf7
John Garner <jdextergarneriv@gmail.com>
2020-11-12 14:46:16 -0500

    setup get function for /api/tables sending json formatted tableData

---
+++ app/routing/api-routes.js
@@ -0,0 +1,8 @@
+var tableData = require("../data/table-data.js");
+var waitingListData = require("../data/waitinglist-data.js");
+
+module.exports = function (app) {
+	app.get("/api/tables", function (req, res) {
+		rest.json(tableData);
+	});
+};
